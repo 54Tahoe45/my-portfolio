636a68170b9cdc533704fa82e908ca3600794ab3
Ryan Moore <ryanmoore@Inkas-MacBook-Pro.local>
2021-03-07 20:09:52 -0800

    entire portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import image from "../santacruz.JPG"
+
+export default function Home() {
+    return (
+        <main>
+            <img 
+                src={ image } 
+                alt="Santa Cruz Sunrise" 
+                className="absolute object-cover w-full h-full" 
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ryan.</h1>
+            </section>
+        </main>    
+    )
+}
